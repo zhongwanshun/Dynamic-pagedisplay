f3281badd26d7ecdfe1d939970643a09817d436b
钟万顺 <1572662085@qq.com>
2021-05-30 13:02:40 +0800

    Dynamic pagedisplay

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var ejs = require('ejs');
 
 var indexRouter = require('./routes/index');
+var aboutRouter = require('./routes/about');
 
 
 var app = express();
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/about', aboutRouter);
 
 
 // catch 404 and forward to error handler
